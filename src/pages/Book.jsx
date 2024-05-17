0eece133aa5b6ff05cd3d0aaea611bfc971782a2
danyalahmad2000 <danigurrero.dg@gmail.com>
2024-05-17 19:02:11 +0500

    Website Complete

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -6,6 +6,7 @@ import heroLeftImg from "../assets/images/hero-left-decorator-removebg-preview.p
 import heroRightImg from "../assets/images/hero-right-decorator-removebg-preview.png";
 import normalBear from "../assets/images/hero-bear-lg.png";
 import stylishBear from "../assets/images/hero-bear-md.png";
+import plusIcon from "../assets/images/tabler-icon-plus.png"
 
 const Book = () => {
   return (
@@ -13,11 +14,11 @@ const Book = () => {
       {/* Hero Section  */}
       <div className="w-full h-full flex flex-row relative">
         <img
-          className="w-[50px] md:w-[234px] md:h-[390px] absolute left-0 top-28 md:top-20"
+          className="w-[50px] md:w-[234px] md:h-[390px] absolute left-0 top-20 md:top-20"
           src={heroLeftImg}
           alt=""
         />
-        <div className="w-full md:w-[858px] gap-[40px] mx-auto mt-[157px] mb-[93px] font-[400] flex flex-col px-2">
+        <div className="w-full md:w-[858px] gap-[40px] mx-auto mt-[157px] mb-[93px] font-[400] flex flex-col px-5 md:px-0">
           <div className="w-full gap-[38px] flex flex-row justify-between">
             <div className="gap-[24px] w-[339px] flex flex-col">
               <img src={normalBear} alt="" />
@@ -25,7 +26,7 @@ const Book = () => {
                 <h1 className="text-[32px] text-opacity-70">Parent X</h1>
               </div>
             </div>
-            <div className="size-[104px]  my-auto"></div>
+            <div className="size-[104px]  my-auto"><img src={plusIcon} alt="" /></div>
             <div className="gap-[24px] w-[339px] flex flex-col">
               <img className="object-cover" src={stylishBear} alt="" />
               <div className="w-[103px] mx-auto">
@@ -39,7 +40,7 @@ const Book = () => {
         </div>
 
         <img
-          className="w-[50px] md:w-[234px] md:h-[390px] absolute right-0 top-28 md:top-20"
+          className="w-[50px] md:w-[234px] md:h-[390px] absolute right-0 top-20 md:top-20"
           src={heroRightImg}
           alt=""
         />
